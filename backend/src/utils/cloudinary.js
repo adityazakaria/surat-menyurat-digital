5f0311d8219b1bee18d497947c6058f7e5c145f3
adityazakaria <adityazakaria97@gmail.com>
2026-06-03 23:01:24 +0700

    Finalize: update env template, fix UI components, prepare for deployment

--- backend/src/utils/cloudinary.js
+++ backend/src/utils/cloudinary.js
@@ -1,4 +1,3 @@
 export const uploadToCloudinary = async (filePath) => {
-  // Dummy - ganti dengan implementasi cloudinary jika diperlukan
   return { secure_url: "http://localhost:5000/uploads/" + filePath };
 };
