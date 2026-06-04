b18ea425de866a01aaa9d7965253a942d987784d
adityazakaria <adityazakaria97@gmail.com>
2026-06-04 22:17:10 +0700

    rubah api fronend

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -3,12 +3,36 @@ import axios from "axios";
 const api = axios.create({
   baseURL:
     "https://surat-menyurat-digital-3yw6-6t3v8rnga-adityaz-s-projects.vercel.app/api",
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) config.headers.Authorization = `Bearer ${token}`;
-  return config;
-});
+// Tambahkan token ke setiap request (jika ada)
+api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+
+    return config;
+  },
+  (error) => Promise.reject(error),
+);
+
+// Jika token tidak valid, logout otomatis
+api.interceptors.response.use(
+  (response) => response,
+  (error) => {
+    if (error.response?.status === 401) {
+      localStorage.removeItem("token");
+      localStorage.removeItem("user");
+    }
+
+    return Promise.reject(error);
+  },
+);
 
 export default api;
